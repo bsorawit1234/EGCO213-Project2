776386d4859b0161a429c46bfb36f4b3026d57ab
PHUPH41622 <143483485+PHUPH41622@users.noreply.github.com>
2023-11-08 17:05:04 +0700

    FactoryThread Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
 import java.io.*;
+import java.sql.SQLOutput;
 import java.util.*;
 import java.lang.*;
 import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static <SuppliersThread> void main(String[] args) {
         String path = "src/main/java/";
         String filename = "config.txt";
         CountDownLatch mainLatch = new CountDownLatch(1); // mainLatch is used for signal other thread to start after mainThread prints day
+        //Exchanger<Material> exchanger = new Exchanger<>();
 
         boolean openSuccess = false;
 
@@ -17,11 +19,14 @@ public class Main {
                 System.out.printf("%-16s>>  read configs from %s\n", Thread.currentThread().getName(), path+filename);
                 openSuccess = true;
 
+
                 String line;
                 String[] col;
 
                 ArrayList<Material> Materials = new ArrayList<Material>();
                 ArrayList<SupplierThread> Suppliers = new ArrayList<SupplierThread>();
+                ArrayList<FactoryThread> Factories = new ArrayList<FactoryThread>();
+
                 int days = 0;
                 int sup_parties = 0;
                 int fac_parties = 0;
@@ -60,19 +65,35 @@ public class Main {
                             f_lot = Integer.parseInt(col[3].trim());
                             f_1 = Integer.parseInt(col[4].trim());
                             f_2 = Integer.parseInt(col[5].trim());
+                            Factories.add(new FactoryThread(fac_name, Materials, f_lot, f_1, f_2));
+                            fac_parties++;
                         }
                     }
                 }
 
+
                 for(SupplierThread s: Suppliers) s.setBarrier(sup_parties);
 
-//                for(int i = 1; i <= days; i++) {
-                    System.out.printf("%-16s>>  %s \n", Thread.currentThread().getName(), "-".repeat(60));
-//                    System.out.printf("%-16s>>  Day %d \n", Thread.currentThread().getName(), i);
-                    for(SupplierThread s: Suppliers) {
-                        s.start();
+                CyclicBarrier barrier = new CyclicBarrier(fac_parties);
+
+
+                    for(int i = 1; i <= days; i++) {
+//                        System.out.printf("%-16s>>  %s \n", Thread.currentThread().getName(), "-".repeat(60));
+//                        System.out.printf("%-16s>>  Day %d \n", Thread.currentThread().getName(), i);
+                        for(SupplierThread s: Suppliers) {
+                            //s.setBarrier(factoryBarrier);
+                            s.start();
+                            s.join();
+                            //try { s.sleep(1000); } catch (Exception e) { }
+                        }
+                        for(FactoryThread f: Factories) {
+                            f.setBarrier(barrier);
+                            f.start();
+                        }
+                        for (FactoryThread f : Factories) {
+                            f.join();
+                        }
                     }
-//                }
             } catch (Exception e) {
                 System.out.println(e);
             }
@@ -130,9 +151,11 @@ class SupplierThread extends Thread {
     }
 
     public void run() {
-        for(int i = 0; i < ML.size(); i++) {
-            ML.get(i).addBalance(sup_rate.get(i));
-            System.out.printf("%-16s>>  %-8s %3d %-10s balance = %3d %s\n", Thread.currentThread().getName(), "Put", sup_rate.get(i), ML.get(i).getName(), ML.get(i).getBalance(), ML.get(i).getName());
+        synchronized (this) {
+            for(int i = 0; i < ML.size(); i++) {
+                ML.get(i).addBalance(sup_rate.get(i));
+                System.out.printf("%-16s>>  %-8s %3d %-10s balance = %3d %s\n", Thread.currentThread().getName(), "Put", sup_rate.get(i), ML.get(i).getName(), ML.get(i).getBalance(), ML.get(i).getName());
+            }
         }
 //        try {
 //            barrier.await(); // signal for start factory thread
@@ -145,5 +168,48 @@ class SupplierThread extends Thread {
 }
 
 class FactoryThread extends Thread {
+    private ArrayList<Material> ML;
+    private ArrayList<Integer> Fbalance = new ArrayList<Integer>();
+    private CyclicBarrier barrier;
+    private int f_lot, f_1, f_2;
+    private Exchanger<Material>  exchanger;
 
+    public void setExchanger(Exchanger<Material> ex)     { exchanger = ex; }
+
+    public FactoryThread(String name, ArrayList<Material> M, int f_lot, int f_1, int f_2) {
+        super(name);
+        ML = M;
+        this.f_lot = f_lot;
+        this.f_1 = f_1;
+        this.f_2 = f_2;
+        for( int i = 0; i < ML.size(); i++ ) {
+            Fbalance.add(0);
+        }
+    }
+
+    public void setBarrier(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            System.out.printf("%s       >>  Holding", Thread.currentThread().getName());
+            for( int i = 0; i < ML.size(); i++ ) {
+                System.out.printf("%5d %s ", Fbalance.get(i), ML.get(i).getName());
+            }
+            System.out.println();
+            int delay1 = (int)(Math.random() * 123);
+            try { Thread.sleep(delay1); } catch(InterruptedException e) { }
+        }
+
+        try { barrier.await(); } catch (Exception e) { }
+
+        for( int i = 0; i < ML.size(); i++ ) {
+            if( f_lot * f_1 < ML.get(0).getBalance()|| f_lot * f_2 < ML.get(1).getBalance()) {
+                Fbalance.add(0, f_lot * f_1);
+                System.out.printf("%s       >>  Get %5s %s             balance = %4d %s\n", Thread.currentThread().getName(), Fbalance.get(i), ML.get(i).getName(), ML.get(i).getBalance(), ML.get(i).getName());
+            }
+        }
+    }
 }
